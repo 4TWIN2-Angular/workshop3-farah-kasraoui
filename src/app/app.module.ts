ecfe1e8d992d1af9691960cd5eef0e602dc3a2a2
farah <farah.kasraoui.40@gmail.com>
2022-09-28 22:41:03 +0200

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SlideComponent } from './slide/slide.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListeuserComponent } from './listeuser/listeuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SlideComponent,
+    FooterComponent,
+    ListeuserComponent
   ],
   imports: [
     BrowserModule,
